7015e2b2e90a075e367cd43e966af1bb68b6ceda
salvatore sorrentino <salvatore.sorrentino@live.com>
2025-05-06 11:58:03 +0200

    DataErrorInfo

---
+++ DemoDataErrorInfo/PersonaViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDataErrorInfo
+{
+    public class PersonaViewModel : INotifyPropertyChanged, IDataErrorInfo
+    {
+        private string _nome;
+
+        [Required(ErrorMessage = "Il nome è obbligatorio")]
+        [MinLength(3, ErrorMessage = "Il nome deve essere lungo almeno 3 caratteri")]
+        public string Nome
+        {
+            get => _nome;
+            set
+            {
+                if (_nome != value)
+                {
+                    _nome = value;
+                    OnPropertyChanged(nameof(Nome));
+                }
+            }
+        }
+
+
+        public string this[string columnName]
+        {
+            get { 
+               var context = new ValidationContext(this) { 
+                   MemberName = columnName };
+               var results = new List<ValidationResult>();
+               bool isValid = Validator.TryValidateProperty(
+                   GetType().GetProperty(columnName).GetValue(this),
+                   context,
+                   results);
+                return results.FirstOrDefault()?.ErrorMessage;
+
+            }
+        }
+            
+            
+            
+
+        public string Error => null;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
