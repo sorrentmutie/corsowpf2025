e1ae7e3465fdf5081747634e1fa222016982532f
Federico Ciancia <ciancia_guest@amada-ise.com>
2025-05-06 17:19:26 +0200

    Added CustomHeight DependencyProperty

--- DemoDependencyProperty/HighlightButton.cs
+++ DemoDependencyProperty/HighlightButton.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace DemoDependencyProperty
 {
-    public class HighlightButton: Button
+    public class HighlightButton : Button
     {
-
-
-
         public bool IsHighlighted
         {
             get { return (bool)GetValue(IsHighlightedProperty); }
@@ -21,9 +14,46 @@ namespace DemoDependencyProperty
 
         // Using a DependencyProperty as the backing store for IsHighlighted.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty IsHighlightedProperty =
-            DependencyProperty.Register("IsHighlighted", typeof(bool), typeof(HighlightButton), new PropertyMetadata(false));
+            DependencyProperty.Register(
+                "IsHighlighted",
+                typeof(bool),
+                typeof(HighlightButton),
+                new PropertyMetadata(false, IsHighlightedPropertyChanged));
 
+        private static void IsHighlightedPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            HighlightButton button = d as HighlightButton;
+            button.IsHighlighted = (bool)e.NewValue;
+            if (!button.IsHighlighted)
+            {
+                button.Height = 200;
+            }
+            else
+            {
+                button.CustomHeight += 50;
+                button.Content = button.CustomHeight;
+            }
+        }
 
+        public int CustomHeight
+        {
+            get { return (int)GetValue(CustomHeightProperty); }
+            set { SetValue(CustomHeightProperty, value); }
+        }
 
+        // Using a DependencyProperty as the backing store for CustomHeight.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CustomHeightProperty =
+            DependencyProperty.Register("CustomHeight", typeof(int), typeof(HighlightButton), new PropertyMetadata(100, IsCustomHeightPropertyChanged));
+
+        private static void IsCustomHeightPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            HighlightButton button = d as HighlightButton;
+            button.CustomHeight = (int)e.NewValue;
+            if (button.CustomHeight >= 300)
+            {
+                button.Background = Brushes.ForestGreen;
+                button.CustomHeight = 0;
+            }
+        }
     }
 }
--- DemoDependencyProperty/MyControl.xaml.cs
+++ DemoDependencyProperty/MyControl.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DemoDependencyProperty
 {
@@ -20,8 +8,6 @@ namespace DemoDependencyProperty
     /// </summary>
     public partial class MyControl : UserControl
     {
-
-
         public string Titolo
         {
             get { return (string)GetValue(TitoloProperty); }
@@ -31,8 +17,8 @@ namespace DemoDependencyProperty
         // Using a DependencyProperty as the backing store for Titolo.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty TitoloProperty =
             DependencyProperty.Register(
-                "Titolo", 
-                typeof(string), 
+                "Titolo",
+                typeof(string),
                 typeof(MyControl), new PropertyMetadata(
                     "TitoloDefault", OnTitoloChanged));
 
