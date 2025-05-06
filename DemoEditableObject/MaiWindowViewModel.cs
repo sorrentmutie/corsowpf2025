bdecfda91d54a7fc270ee5f6c44cb87e2d5010c1
salvatore sorrentino <salvatore.sorrentino@live.com>
2025-05-06 12:46:15 +0200

    Editable Object

---
+++ DemoEditableObject/MaiWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEditableObject
+{
+    public class MaiWindowViewModel
+    {
+        public ObservableCollection<Persona> Persone { get; set; }
+
+        public MaiWindowViewModel()
+        {
+            Persone = new ObservableCollection<Persona>
+            {
+                new Persona { Nome = "Mario" },
+                new Persona { Nome = "Luigi" },
+                new Persona { Nome = "Peach" }
+            };
+        }
+    }
+}
---
+++ DemoEditableObject/Persona.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEditableObject
+{
+    public class Persona : INotifyPropertyChanged, IEditableObject
+    {
+        private string _nome;
+        private string _backupNome;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (_nome != value)
+                {
+                    _nome = value;
+                    OnPropertyChanged(nameof(Nome));
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void BeginEdit()
+        {
+            _backupNome = Nome;
+        }
+
+        public void CancelEdit()
+        {
+            Nome = _backupNome;
+        }
+
+        public void EndEdit()
+        {
+            _backupNome = "";
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
