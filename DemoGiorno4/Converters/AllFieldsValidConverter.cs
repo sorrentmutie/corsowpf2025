2e45c73efcf00dbf891f6ad25319c3ca13056994
salvatore sorrentino <salvatore.sorrentino@live.com>
2025-05-06 10:57:45 +0200

    multivalue

---
+++ DemoGiorno4/Converters/AllFieldsValidConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DemoGiorno4.Converters
+{
+    public class AllFieldsValidConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            string text = values[0] as string;
+            bool isChecked = values[1] is bool b && b;
+
+            return !string.IsNullOrEmpty(text) && isChecked;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
