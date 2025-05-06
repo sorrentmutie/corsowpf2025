2afbb1c5c920e269e8c4323686a7cde05f8934dd
salvatore sorrentino <salvatore.sorrentino@live.com>
2025-05-06 16:49:13 +0200

    dep property

---
+++ DemoDependencyProperty/HighlightButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DemoDependencyProperty
+{
+    public class HighlightButton: Button
+    {
+
+
+
+        public bool IsHighlighted
+        {
+            get { return (bool)GetValue(IsHighlightedProperty); }
+            set { SetValue(IsHighlightedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsHighlighted.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsHighlightedProperty =
+            DependencyProperty.Register("IsHighlighted", typeof(bool), typeof(HighlightButton), new PropertyMetadata(false));
+
+
+
+    }
+}
---
+++ DemoDependencyProperty/MyControl.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoDependencyProperty
+{
+    /// <summary>
+    /// Logica di interazione per MyControl.xaml
+    /// </summary>
+    public partial class MyControl : UserControl
+    {
+
+
+        public string Titolo
+        {
+            get { return (string)GetValue(TitoloProperty); }
+            set { SetValue(TitoloProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Titolo.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitoloProperty =
+            DependencyProperty.Register(
+                "Titolo", 
+                typeof(string), 
+                typeof(MyControl), new PropertyMetadata(
+                    "TitoloDefault", OnTitoloChanged));
+
+        private static void OnTitoloChanged(
+            DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as MyControl;
+            string nuovoTitolo = e.NewValue as string;
+            control.MyLabel.Content = nuovoTitolo;
+            // Fai qualcosa con titolo
+        }
+
+        public MyControl()
+        {
+            InitializeComponent();
+            MyLabel.Content = Titolo;
+        }
+
+    }
+}
