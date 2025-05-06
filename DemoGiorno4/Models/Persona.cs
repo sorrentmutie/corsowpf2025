8734007d3f3d5b57df57886729397addcfbcc168
Federico Ciancia <ciancia_guest@amada-ise.com>
2025-05-06 10:39:52 +0200

    Modified new module VM and model

--- DemoGiorno4/Models/Persona.cs
+++ DemoGiorno4/Models/Persona.cs
@@ -1,15 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace DemoGiorno4.Models
 {
-    public class Persona
+    public class Persona : INotifyPropertyChanged
     {
-        public string Nome { get; set; }
-        public string Cognome { get; set; }
-        public string Email { get; set; }
+        private string _nome;
+        private string _cognome;
+        private string _email;
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                if (value == string.Empty || value != _nome)
+                {
+                    _nome = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Nome)));
+                }
+            }
+        }
+        public string Cognome
+        {
+            get
+            {
+                return _cognome;
+            }
+            set
+            {
+                if (value == string.Empty || value != _cognome)
+                {
+                    _cognome = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Cognome)));
+                }
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                if (value == string.Empty || value != _email)
+                {
+                    _email = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Email)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Persona()
+        {
+            Nome = string.Empty;
+            Cognome = string.Empty;
+            Email = string.Empty;
+        }
     }
 }
--- DemoGiorno4/ViewModels/MainWindowViewModel.cs
+++ DemoGiorno4/ViewModels/MainWindowViewModel.cs
@@ -1,18 +1,31 @@
 ﻿using DemoGiorno4.Models;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using WpfApp2.Helpers;
 
 namespace DemoGiorno4.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
+        private Persona _nuovaPersona = new Persona();
         public ObservableCollection<Persona> Persone { get; set; }
         public RelayCommand<Persona> EliminaPersonaCommand { get; }
+        public RelayCommand<Persona> AggiungiPersonaCommand { get; }
+        public Persona NuovaPersona
+        {
+            get
+            {
+                return _nuovaPersona;
+            }
+            set
+            {
+                if (value != _nuovaPersona)
+                {
+                    _nuovaPersona = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NuovaPersona)));
+                }
+            }
+        }
 
 
         public MainWindowViewModel()
@@ -23,21 +36,27 @@ namespace DemoGiorno4.ViewModels
                 new Persona() { Nome = "Luigi", Cognome = "Bianchi", Email = "b@gmail.com"},
             };
 
-            EliminaPersonaCommand = new RelayCommand<Persona>(
-                  EliminaPersona, CanEliminaPersona);
-
+            EliminaPersonaCommand = new RelayCommand<Persona>(EliminaPersona);
+            AggiungiPersonaCommand = new RelayCommand<Persona>(AggiungiPersona);
         }
 
-        private bool CanEliminaPersona(Persona persona)
-        {
-            return persona != null;
-        }
+        public event PropertyChangedEventHandler PropertyChanged;
 
         private void EliminaPersona(Persona persona)
         {
-            if (persona != null && Persone.Contains(persona)) { 
+            if (persona != null && Persone.Contains(persona))
+            {
                 Persone.Remove(persona);
-            };
+            }
+        }
+
+        private void AggiungiPersona(Persona persona)
+        {
+            if (persona != null)
+            {
+                Persone.Add(persona);
+                NuovaPersona = new Persona();
+            }
         }
     }
 }
