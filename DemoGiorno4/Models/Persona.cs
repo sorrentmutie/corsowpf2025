19b824d61d95932b28e529909088e4000bb46b9f
salvatore sorrentino <salvatore.sorrentino@live.com>
2025-05-06 10:04:19 +0200

    Command Generico

---
+++ DemoGiorno4/Models/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGiorno4.Models
+{
+    public class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ DemoGiorno4/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using DemoGiorno4.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp2.Helpers;
+
+namespace DemoGiorno4.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public ObservableCollection<Persona> Persone { get; set; }
+        public RelayCommand<Persona> EliminaPersonaCommand { get; }
+
+
+        public MainWindowViewModel()
+        {
+            Persone = new ObservableCollection<Persona>()
+            {
+                new Persona() { Nome = "Mario", Cognome = "Rossi", Email = "m@gmail.com"},
+                new Persona() { Nome = "Luigi", Cognome = "Bianchi", Email = "b@gmail.com"},
+            };
+
+            EliminaPersonaCommand = new RelayCommand<Persona>(
+                  EliminaPersona, CanEliminaPersona);
+
+        }
+
+        private bool CanEliminaPersona(Persona persona)
+        {
+            return persona != null;
+        }
+
+        private void EliminaPersona(Persona persona)
+        {
+            if (persona != null && Persone.Contains(persona)) { 
+                Persone.Remove(persona);
+            };
+        }
+    }
+}
